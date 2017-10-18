c29c2cf33056eb5e0843fd0e8b7d72aacedd7c4c
Mao <andresmao23@hotmail.com>
2017-10-17 21:19:37 -0500

    Vista grid realizada

--- js/index.js
+++ js/index.js
@@ -3,13 +3,15 @@ var chilangoRef = database.ref("chilango");
 var cancionesChild = chilangoRef.child("canciones");
 var saludosChild = chilangoRef.child("saludos");
 
+//var cajaSaludos = document.getElementById('saludos');
+
 cancionesChild.on('value', function(snapshot){        
-    $("#canciones").html(""); // Limpiamos el cotenedor de canciones
+    $("#canciones-dos").html(""); // Limpiamos el cotenedor de canciones
     snapshot.forEach(function(e){
         var obj = e.val();
         if(obj.nombre!=null){
             console.log("Canción: "+ obj.nombre);
-            $("#canciones").append('<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
+            $("#canciones-dos").append('<section class="mdl-cell mdl-cell--4-col section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
                     '<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone"><div class="mdl-card__supporting-text"><h2>Nombre: '+ 
                     obj.nombre + "</h2><h2>Autor: "+ 
                     obj.autor + "</h2><h2>Votos: "+ 
@@ -20,12 +22,12 @@ cancionesChild.on('value', function(snapshot){
 });
 
 saludosChild.on('value', function(snapshot){        
-    $("#saludos").html(""); // Limpiamos el cotenedor de saludos
+    $("#saludos-dos").html(""); // Limpiamos el cotenedor de saludos
     snapshot.forEach(function(e){
         var obj = e.val();
         if(obj.url!=null){
             console.log("Saludos: "+ obj.detalle);
-            $("#saludos").append('<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
+            $("#saludos-dos").append('<section class="mdl-cell mdl-cell--4-col section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
                     '<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone"><div class="mdl-card__supporting-text"><h4>Emisor: '+ 
                     obj.emisor + "</h4><h4>Receptor: "+ 
                     obj.receptor + "</h4><h4>Detalle: "+ 
@@ -34,7 +36,7 @@ saludosChild.on('value', function(snapshot){
                     '<header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white"><button onclick="removerSaludo(\''+e.key+'\')" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">delete_forever</i></button></header></section>');
         }else{
             console.log("Saludos: "+ obj.detalle);
-            $("#saludos").append('<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
+            $("#saludos-dos").append('<section class="mdl-cell mdl-cell--4-col section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
                     '<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone"><div class="mdl-card__supporting-text"><h4>Emisor: '+ 
                     obj.emisor + "</h4><h4>Receptor: "+ 
                     obj.receptor + "</h4><h4>Detalle: "+ 
@@ -45,6 +47,19 @@ saludosChild.on('value', function(snapshot){
         }
     });
 });
+
+/*saludosChild.on('child_changed', function(snapshot) {
+  console.log(snapshot.val());
+  var obj = snapshot.val();
+  cajaSaludos.innerHTML = '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">' +
+                    '<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone"><div class="mdl-card__supporting-text"><h4>Emisor: '+ 
+                    obj.emisor + "</h4><h4>Receptor: "+ 
+                    obj.receptor + "</h4><h4>Detalle: "+ 
+                    obj.detalle + "</h4></div>" +
+                    '<img src="'+ obj.url +'" class="imagen"></div>' +
+                    '<header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white"><button onclick="removerSaludo(\''+snapshot.key+'\')" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"><i class="material-icons">delete_forever</i></button></header></section>' + cajaSaludos.innerHTML;
+  console.log(cajaSaludos);
+});*/
 
 function remover(key){
     console.log("Key: "+ key);
